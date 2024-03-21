ffb2c4cefb08b1a7347846ac24969663735d3ffe
root <2450094803@qq.com>
2024-03-21 09:34:06 +0800

    刘锦涛职位管理的更改

--- oa-system-bean/src/main/java/org/hret/entity/personnel/leave/Recording.java
+++ oa-system-bean/src/main/java/org/hret/entity/personnel/leave/Recording.java
@@ -5,11 +5,13 @@ import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.io.Serializable;
 import java.util.Date;
+@EqualsAndHashCode(callSuper = true)
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
