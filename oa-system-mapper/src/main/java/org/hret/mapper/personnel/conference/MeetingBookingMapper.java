cfe0868e5d7ab11d9513fdb71dde876745fdd209
root <2450094803@qq.com>
2024-04-10 16:55:36 +0800

    会议室模块上传

---
+++ oa-system-mapper/src/main/java/org/hret/mapper/personnel/conference/MeetingBookingMapper.java
@@ -0,0 +1,14 @@
+package org.hret.mapper.personnel.conference;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+import org.hret.entity.personnel.conference.MeetingBooking;
+
+/**
+ * Authar:liujintao
+ * Data:2024/4/10
+ * jdk:17
+ */
+@Mapper
+public interface MeetingBookingMapper extends BaseMapper<MeetingBooking> {
+}
