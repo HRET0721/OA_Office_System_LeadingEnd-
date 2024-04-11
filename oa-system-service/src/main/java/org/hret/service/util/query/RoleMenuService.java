04542b81f1ed446301b44e85ff5cd85a75870f5c
HRET <2126366332@qq.com>
2024-04-11 15:03:39 +0800

    角色权限关联

---
+++ oa-system-service/src/main/java/org/hret/service/util/query/RoleMenuService.java
@@ -0,0 +1,13 @@
+package org.hret.service.util.query;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import org.hret.entity.utils.query.RoleMenu;
+
+/**
+ * @author HRET Milky Way
+ * @version 1.0
+ * Date:2024/4/11
+ * description TODO
+ */
+public interface RoleMenuService extends IService<RoleMenu> {
+}
